10f28987a2a3246e8b2f713922bb8312d9d45bd7
dfs700 (Dominik Fuchs) <contact@dominikfuchs.com>
2009-04-09 22:21:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spsextante@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/spsextante-base.R
@@ -0,0 +1,18 @@
+spsextante.helloworld <- function()
+{
+   spx <- .jnew("spsextante")          # create instance of spsextante class
+   out <- .jcall(spx, "S", "sayHello") # invoke sayHello method
+   return(out)
+}
+
+spsextante.init = function(env = spsextante.env())
+{
+}
+
+spsextante.env = function(classpath)
+{
+}
+
+spsextante.runalg = function(algorithm, param=list())
+{
+}
