01b2a009d4130553ac6859565dbdbdee0e37c691
dfs700 (Dominik Fuchs) <contact@dominikfuchs.com>
2009-04-09 22:20:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spsextante@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/java/spsextante.java
@@ -0,0 +1,14 @@
+public class spsextante
+{
+   
+   public String sayHello()
+   {
+      String result = new String("Hello Java World!");
+      return result;
+   }
+
+   public static void main(String[] args)
+   {
+   }
+
+} /* end class spsextante */
